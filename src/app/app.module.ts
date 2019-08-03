6c56a4c23bfd990dc275e46f39c6067d666a84b3
Rodrigo55pereira <rodrigo55pereira@gmail.com>
2019-08-03 11:43:21 -0300

    atualização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MyFirstComponent } from './myfirst.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/myfirst.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'my-first',
+    templateUrl: './myfirst.component.html',
+    styleUrls: ['./myfirst.component.css']
+})
+export class MyFirstComponent{
+
+}
